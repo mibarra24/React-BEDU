be715ebbdfc80851409711bd7d495711eb2b349d
marco <marcodzeko@gmail.com>
2023-11-04 01:52:03 -0700

    primer clase

---
+++ primera-clase-vite/src/components/Form.jsx
@@ -0,0 +1,10 @@
+const Form = () => {
+    return (
+        <Form>
+            <input placeholder="Capture la tarea a ejecutar"/>
+            <button>Agregar Tarea</button>
+        </Form>
+    )
+}
+
+export default Form;
---
+++ primera-clase-vite/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import '../styles/header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="card-header-title header">
+                Hay 0 tareas
+            </h1>
+        </div>
+    )
+};
+
+export default Header;
